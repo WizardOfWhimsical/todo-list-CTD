0cd3a08a4d06fa6aa82f13bcaa331675afadf4f9
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-05 20:17:14 -0500

    added form input to ToDoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import ToDoList from './todolist';
+import ToDoForm from './ToDoForm';
 
 function App() {
   return (
     <>
       <h1>My Todos</h1>
+      <ToDoForm />
       <ToDoList />
     </>
   );
---
+++ src/ToDoForm.jsx
@@ -0,0 +1,12 @@
+function ToDoForm() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="todoTitle">To Do: </label>
+        <input type="input" id="todoTitle" />
+      </div>
+    </form>
+  );
+}
+
+export default ToDoForm;
