0c18dda998cd79838e23673f02def8886e6f7f7d
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-10 17:03:25 -0500

    fix: put array in ToDoList component via instructions, why not outside it above the function decleration?

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,10 +1,10 @@
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
-
 function ToDoList() {
+  const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
+
   return (
     <ul>
       {todos.map((todo) => (
