e331876eea21a0c30359fa4fe6e80743c5eadb52
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-26 14:53:18 -0500

    added empty array to useState, also removed previous array

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,9 @@ import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 import { useState } from 'react';
 
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
-
 function App() {
-  const [toDoList, setToDoList] = useState(todos);
+  const [toDoList, setToDoList] = useState([]);
+
   return (
     <>
       <h1>My Todos</h1>
