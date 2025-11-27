43cddd5ce08a29d53e998b8a0b5eb2f2eb43a5a6
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-27 18:27:16 -0500

    feat: set up form the way they wanted { * o * }

--- src/ToDoForm.jsx
+++ src/ToDoForm.jsx
@@ -1,6 +1,22 @@
 import { useRef } from 'react';
 
-function ToDoForm() {
+function ToDoForm({ onAddTodo }) {
+  const inputRef = useRef();
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+
+    console.log(event.target.todoTitle.value);
+    //trim is always smart.
+    const todoTitle = event.target.todoTitle.value.trim();
+    //check for empty string
+    if (todoTitle) {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus(); //<-whats this do?
+    }
+  };
+
   return (
     <form>
       <label htmlFor="todoTitle">To Do: </label>
