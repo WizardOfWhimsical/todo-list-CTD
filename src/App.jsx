a50b2c1fc88e647522330362311b94856074739b
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-27 18:14:42 -0500

    set up App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import './App.css';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 import { useState } from 'react';
+//kept in for baseline
+const todos = [
+  { id: 1, title: 'review resources' },
+  { id: 2, title: 'take notes' },
+  { id: 3, title: 'code out app' },
+];
 
 function App() {
-  const [toDoList, setToDoList] = useState([]);
-
-  function addTodo(todoTitle) {
-    const newTodo = { id: new Date(), title: todoTitle };
-    return setToDoList([newTodo, ...toDoList]);
+  const [todoList, setToDoList] = useState(todos);
+  /**
+   * @param {string} todoTitle
+   */
+  function addToDo(todoTitle) {
+    const newToDo = { id: new Date.now(), title: todoTitle };
+    //when passing in the first parameter, it is like recieving a promise
+    //this gives us the current list (previousTodo) to add new item to the list
+    setToDoList((previousTodos) => [newToDo, ...previousTodos]);
   }
-
   return (
     <>
       <h1>My Todos</h1>
-      <ToDoForm onAddToDo={addTodo} />
-      <ToDoList todos={toDoList} />
+      <ToDoForm onAddTodo={addToDo} />
+      <ToDoList todos={todoList} />
     </>
   );
 }
