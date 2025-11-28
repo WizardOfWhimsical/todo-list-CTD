b7354671c9c40d056c9fc21cf463b25de59746db
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-11-28 10:30:33 -0500

    chore: removed previous useState(todos) for empry array to test functionality as i continue

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 import { useState } from 'react';
 //kept in for baseline
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
+// const todos = [
+//   { id: 1, title: 'review resources' },
+//   { id: 2, title: 'take notes' },
+//   { id: 3, title: 'code out app' },
+// ];
 
 function App() {
-  const [todoList, setToDoList] = useState(todos);
+  const [todoList, setToDoList] = useState([]);
+  // const [todoList, setToDoList] = useState(todos);
+
   /**
    * @param {string} todoTitle
    */
@@ -21,6 +23,7 @@ function App() {
     //this gives us the current list (previousTodo) to add new item to the list
     setToDoList((previousTodos) => [newToDo, ...previousTodos]);
   }
+
   return (
     <>
       <h1>My Todos</h1>
