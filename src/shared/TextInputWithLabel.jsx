fb2ed35e187c4c6fa9060d91d51788bf9415ff6c
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-12-03 14:57:41 -0500

    feat: created reusable cinput component field

---
+++ src/shared/TextInputWithLabel.jsx
@@ -0,0 +1,20 @@
+export default function TextInputWithLabel({
+  elementId,
+  labelText,
+  onChange,
+  ref,
+  value,
+}) {
+  return (
+    <>
+      <label htmlFor={elementId}>{labelText}</label>
+      <input
+        type="text"
+        id={elementId}
+        value={value}
+        ref={ref}
+        onChange={onChange}
+      />
+    </>
+  );
+}
