49cd5120d263453652d660f0c8b7f27dfb0ba31a
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-12-03 15:58:13 -0500

    chore: cleaned up form for a break

--- src/features/ToDoForm.jsx
+++ src/features/ToDoForm.jsx
@@ -1,23 +1,19 @@
 import { useRef, useState } from 'react';
 import TextInputWithLabel from '../shared/TextInputWithLabel';
 import { isValidTodoTitle } from '../utils/todoValidation';
-function ToDoForm({ onAddTodo }) {
+
+export default function ToDoForm({ onAddTodo }) {
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   const inputRef = useRef();
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-
-    //trim is always smart.
     const todoTitle = workingTodoTitle.trim();
-    //check for empty string
-    //theres form validation in here??
+
     if (todoTitle) {
       onAddTodo(todoTitle);
-      console.log('1st', event);
       setWorkingTodoTitle('');
-      // inputRef.current.reset();
-      inputRef.current.focus(); //<-puts cursor into input
+      inputRef.current.focus();
     }
   };
 
@@ -31,21 +27,9 @@ function ToDoForm({ onAddTodo }) {
         onChange={(e) => setWorkingTodoTitle(e.target.value)}
         ref={inputRef}
       />
-      {/* <label htmlFor="todoTitle">To Do: </label>
-      <input
-        type="text"
-        id="todoTitle"
-        name="todoTitle"
-        value={workingTodoTitle}
-        onChange={(event) => setWorkingTodoTitle(event.target.value)}
-        ref={inputRef}
-        required
-      /> */}
       <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>
         Add ToDo
       </button>
     </form>
   );
 }
-
-export default ToDoForm;
