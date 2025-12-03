7fb0264061f36cbbbf48a05a6045a45c36c1b2f5
WizardOfWhimsical <st.rayis1085@gmail.com>
2025-12-03 15:28:46 -0500

    feat: added edit feature to do list item

--- src/features/TodoList/ToDoListItem.jsx
+++ src/features/TodoList/ToDoListItem.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
 //ToDoListItem.jsx
 export function ToDoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          onChange={() => onCompleteTodo(todo.id)}
-          checked={todo.isCompleted}
-        />
-        {todo.title}
-      </label>
+      <form>
+        {isEditing ? (
+          <TextInputWithLabel value={todo.title} />
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => onCompleteTodo(todo.id)}
+                checked={todo.isCompleted}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
+      </form>
     </li>
   );
 }
