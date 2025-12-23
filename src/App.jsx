830c58c690b657b7e622598b0b2c6bfef0e8039e
Stephen Lewis <st.rayis1085@gmail.com>
2025-12-23 12:44:53 -0500

    chore: put together components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 //App.jsx
 import './App.css';
 import TodosPage from './features/Todos/TodosPage';
+import Header from './shared/Header';
 
 function App() {
-  console.log('hello world');
-  return <TodosPage />;
+  return (
+    <>
+      <Header />
+      <TodosPage />
+    </>
+  );
 }
 
 export default App;
