7e51160ba791e1c69a1ac4a3b3a6e708ffe660be
Stephen Lewis <st.rayis1085@gmail.com>
2025-12-23 12:16:49 -0500

    chore/feat: moved all todos from app.jsx to new todospage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,9 @@
 //App.jsx
 import './App.css';
-import ToDoList from './features/TodoList/ToDoList';
-import ToDoForm from './features/ToDoForm';
-import { useState } from 'react';
-//kept in for baseline
-const todos = [
-  { id: 1, title: 'review resources', isCompleted: false },
-  { id: 2, title: 'take notes', isCompleted: true },
-  { id: 3, title: 'code out app', isCompleted: false },
-];
+import TodosPage from './features/Todos/TodosPage';
 
 function App() {
-  // const [todoList, setToDoList] = useState([]);
-  const [todoList, setToDoList] = useState(todos);
-
-  /**
-   * @param {string} todoTitle
-   */
-  function addToDo(todoTitle) {
-    const newToDo = { id: Date.now(), title: todoTitle, isCompleted: false };
-    //when passing in the first parameter, it is like recieving a promise
-    //this gives us the current list (previousTodo) to add new item to the list
-    setToDoList((previousTodos) => [newToDo, ...previousTodos]);
-  }
-
-  function completeTodo(todoId) {
-    setToDoList((previousTodos) => {
-      return previousTodos.map((todo) => {
-        if (todo.id === todoId) {
-          return { ...todo, isCompleted: !todo.isCompleted };
-        }
-        return todo;
-      });
-    });
-  }
-
-  function updateTodo(editedTodo) {
-    setToDoList((previousTodos) => {
-      return previousTodos.map((todo) => {
-        if (todo.id === editedTodo.id) {
-          return { ...todo, title: editedTodo.title };
-        }
-        return todo;
-      });
-    });
-  }
-
-  return (
-    <>
-      <h1>My Todos</h1>
-      <ToDoForm onAddTodo={addToDo} />
-      <ToDoList
-        onUpdateTodo={updateTodo}
-        onCompleteTodo={completeTodo}
-        todos={todoList}
-      />
-    </>
-  );
+  <TodosPage />;
 }
 
 export default App;
