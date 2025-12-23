b7b920fd87bb9298257a6f3ed488eaaa91ac728f
Stephen Lewis <st.rayis1085@gmail.com>
2025-12-23 12:40:25 -0500

    fix: added return in app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 import TodosPage from './features/Todos/TodosPage';
 
 function App() {
-  <TodosPage />;
+  console.log('hello world');
+  return <TodosPage />;
 }
 
 export default App;
