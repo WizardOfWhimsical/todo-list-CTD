0ba5d9b2bf4051be8597781fc6bbf87f0f63e9ab
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-10 18:06:17 -0500

    feat: dinky dropdown made

--- src/shared/SortBy.jsx
+++ src/shared/SortBy.jsx
@@ -8,32 +8,16 @@ export default function SortBy({
     <>
       <label for="hr-select">Your favorite food</label> <br />
       <select name="foods" id="hr-select">
-        <option value="">Choose a food</option>
+        <option value="">Sort</option>
         <hr />
-        <optgroup label="Fruit">
-          <option value="apple">Apples</option>
-          <option value="banana">Bananas</option>
-          <option value="cherry">Cherries</option>
-          <option value="damson">Damsons</option>
+        <optgroup label="Sort By">
+          <option value="creationDate">Creation Date</option>
+          <option value="title">Title</option>
         </optgroup>
         <hr />
-        <optgroup label="Vegetables">
-          <option value="artichoke">Artichokes</option>
-          <option value="broccoli">Broccoli</option>
-          <option value="cabbage">Cabbages</option>
-        </optgroup>
-        <hr />
-        <optgroup label="Meat">
-          <option value="beef">Beef</option>
-          <option value="chicken">Chicken</option>
-          <option value="pork">Pork</option>
-        </optgroup>
-        <hr />
-        <optgroup label="Fish">
-          <option value="cod">Cod</option>
-          <option value="haddock">Haddock</option>
-          <option value="salmon">Salmon</option>
-          <option value="turbot">Turbot</option>
+        <optgroup label="Order">
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
         </optgroup>
       </select>
     </>
