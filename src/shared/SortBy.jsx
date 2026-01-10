907c36a6c49b3bec73f3430a82dd91dd8d2341b7
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-10 18:24:54 -0500

    chore: added name attribute

--- src/shared/SortBy.jsx
+++ src/shared/SortBy.jsx
@@ -6,14 +6,13 @@ export default function SortBy({
 }) {
   return (
     <>
-      {/* <label for="hr-select">Your favorite food</label> <br /> */}
       <label htmlFor="sortBy">Sort By</label>
-      <select id="sortBy">
+      <select name="sortBy" id="sortBy">
         <option value="creationDate">Creation Date</option>
         <option value="title">Title</option>
       </select>
       <label htmlFor="order">Order</label>
-      <select id="order">
+      <select name="order" id="order">
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
