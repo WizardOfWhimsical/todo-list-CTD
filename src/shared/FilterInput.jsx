a41af6b3c3b9998ba8fdf6fb88c207cde7169496
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-12 17:05:38 -0500

    feat:created filterInput component

--- src/shared/FilterInput.jsx
+++ src/shared/FilterInput.jsx
@@ -7,6 +7,7 @@ export function FilterInput({ filterTerm, onFilterChange }) {
         type="text"
         value={filterTerm}
         onChange={(e) => onFilterChange(e.target.value)}
+        placeholder="search todos here..."
       />
     </>
   );
