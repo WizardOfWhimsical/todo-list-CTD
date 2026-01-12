a9853e9ede1b0db25f66eb2ed818d3de8e72fe20
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-12 17:00:32 -0500

    feat:created filterInput component

---
+++ src/shared/FilterInput.jsx
@@ -0,0 +1,13 @@
+export function FilterInput({ filterTerm, onFilterChange }) {
+  return (
+    <>
+      <label htmlFor="filterInput">Search Todo's:</label>
+      <input
+        id="filterInput"
+        type="text"
+        value={filterTerm}
+        onChange={(e) => onFilterChange(e.target.value)}
+      />
+    </>
+  );
+}
