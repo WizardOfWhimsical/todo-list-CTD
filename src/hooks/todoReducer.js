7479a55bc507d6a423c326fa2767cf8372e441c9
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-19 07:36:29 -0500

    chore:added comments to reducer so i can aviod repeating code

--- src/hooks/todoReducer.js
+++ src/hooks/todoReducer.js
@@ -1,19 +1,28 @@
 export default function todoReducer(state, action) {
   switch (action.type) {
+    //gets list
     case 'GET_TODOS': {
       return action.data;
     }
+    // puts todo in list
     case 'ADD_TODO': {
       const newToDo = {
         id: action.id,
         title: action.title,
         isCompleted: false,
       };
-
       return [newToDo, ...state];
     }
-
-    // try to turn this into UPDATE_TODO
+    // used for isComplete/isEditing
+    case 'UPDATE_TODO': {
+      return state.map((todo) => {
+        if (todo.id === action.todo.id) {
+          return { ...todo, ...action.todo };
+        }
+        return todo;
+      });
+    }
+    // adds successful db updating to list
     case 'SYNCHRONIZE_TODO': {
       return state.map((todo) => {
         if (todo.id === action.id) {
@@ -22,17 +31,9 @@ export default function todoReducer(state, action) {
         return todo;
       });
     }
-
+    // removes upon failure to update db
     case 'REVERT_ADD_TODO': {
       return state.filter((todo) => todo.id !== action.id);
-    }
-    case 'UPDATE_TODO': {
-      return state.map((todo) => {
-        if (todo.id === action.todo.id) {
-          return { ...todo, ...action.todo };
-        }
-        return todo;
-      });
     }
     default: {
       return state;
