238611dce914d1712444d04545cfa9c9bfc2040e
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-20 15:50:33 -0500

    feat: made custom Auth hook

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState } from 'react';
+//create context
+const AuthContext = createContext();
+
+// custom hook with error checking
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
