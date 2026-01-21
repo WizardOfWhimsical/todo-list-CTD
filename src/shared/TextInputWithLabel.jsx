aee3c90b7d865c5c018c7c248ab48afe389ee5f5
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-21 12:51:59 -0500

    chore: added Form components to filterInput.jsx

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,5 +1,5 @@
 import { useId } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 export default function TextInputWithLabel({
   labelText,
