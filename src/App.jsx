3ad20fcd001c3496e12bfa1036e969d56b907d0e
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-26 13:17:11 -0500

    feat: added Routes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router';
 import TodosPage from './features/Todos/TodosPage';
 import Header from './shared/Header';
 import Logon from './features/Logon';
@@ -7,19 +8,30 @@ import { useAuth } from './context/AuthContext';
 import './App.css';
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);`
-
-  const { token } = useAuth();
-
-  return token ? (
+  const { isAuthenticated } = useAuth();
+  const TODOSPAGE = (
     <>
       <Header message={'Going To Do...'} />
       <TodosPage />
     </>
-  ) : (
+  );
+  const LOGONPAGE = (
     <>
       <Header message={'Please Log In'} />
       <Logon />
+    </>
+  );
+  // i want to nest this or give them there own page. loggin in isnt the same as home page
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={isAuthenticated ? TODOSPAGE : LOGONPAGE} />
+        {/* <Route path="/about" element={} />
+        <Route path="/login" element={} />
+        <Route path="/todos" element={} />
+        <Route path="/profile" element={} />
+        <Route path="*" element={} /> */}
+      </Routes>
     </>
   );
 }
