3a943c49d14b9f89b1d11425ac08527fd045c987
Stephen Lewis <st.rayis1085@gmail.com>
2026-01-28 19:23:51 -0500

    feat: made basics to see data returned in console so i know how to procced

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { get } from '../utils/api';
+import { useAuth } from '../context/AuthContext';
 
 import Button from 'react-bootstrap/Button';
-// do i have a token proble. i see know reason why this page would take me back. is it requireAuth redirecting?
+
 export default function ProfilePage() {
+  const [total, setTotal] = useState(0);
+  const [complete, setComplete] = useState(0);
+  const [active, setActive] = useState(0);
+  const [error, setError] = useState('');
+  const [isloading, setIsLoading] = useState(false);
+
+  const { email, token } = useAuth();
+
   useEffect(() => {
-    async function fetchData() {
-      const data = await get(`tasks`);
-      console.log(data);
+    if (!token) return;
+    let firstPost = false;
+
+    async function fetchTodos() {
+      const options = {
+        headers: { 'X-CSRF-TOKEN': token },
+      };
+      try {
+        const data = await get(`tasks`, options);
+        console.log(data);
+        if (!firstPost) {
+          // put success here
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
-    fetchData();
+
+    fetchTodos();
+
+    return () => {
+      console.log('one render ran clean up');
+      firstPost = true;
+    };
   }, []);
 
   return (
